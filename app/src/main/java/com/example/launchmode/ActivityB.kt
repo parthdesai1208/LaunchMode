3e935c239580995b0a9250fc68fc519222d5cc1f
parthdesai1208 <dparth78@gmail.com>
2023-03-24 18:35:00 +0530

    add taskId in log

--- app/src/main/java/com/example/launchmode/ActivityB.kt
+++ app/src/main/java/com/example/launchmode/ActivityB.kt
@@ -8,7 +8,7 @@ import com.example.launchmode.databinding.ActivityABinding
 import com.example.launchmode.databinding.ActivityBBinding
 
 class ActivityB : AppCompatActivity() {
-    lateinit var binding : ActivityBBinding
+    lateinit var binding: ActivityBBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,23 +16,39 @@ class ActivityB : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnB.setOnClickListener {
-            val intent = Intent(this,ActivityC::class.java)
+            val intent = Intent(this, ActivityC::class.java)
             startActivity(intent)
         }
 
-       /* binding.againCallItself.setOnClickListener {
-            val intent = Intent(this,ActivityB::class.java)
-            startActivity(intent)
-        }*/
+        /* binding.againCallItself.setOnClickListener {
+             val intent = Intent(this,ActivityB::class.java)
+             startActivity(intent)
+         }*/
+        Log.e("Launch", "onCreate-B -- TASK ID: $taskId")
     }
 
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
-        Log.e("Launch","Activity B : onNewIntent")
+        Log.e("Launch", "Activity B : onNewIntent")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.e("Launch","Activity B : onResume")
+        Log.e("Launch", "Activity B : onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e("Launch", "Activity B : onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e("Launch", "Activity B : onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("Launch", "Activity B : onDestroy")
     }
 }
